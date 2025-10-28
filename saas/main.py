ab094546d61337009a0268e51bf9304c0715b706
slickofficials <158083468+slickofficials@users.noreply.github.com>
2025-10-28 01:12:44 +0100

    Create main.py

---
+++ saas/main.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+@login_required
+def dashboard():
+    company = current_user.subdomain.upper() + " HQ"
+    return render_template('saas/dashboard.html', company=company, user=current_user) 
