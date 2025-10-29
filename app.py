7aceea196130cb387b1119079d20c73ef92ad24f
slickofficials <158083468+slickofficials@users.noreply.github.com>
2025-10-29 20:15:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -159,6 +159,13 @@ def youtube_callback():
 @app.route('/terms')
 def terms():
     return render_template('terms.html')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html') 
 
 @app.route('/privacy')
 def privacy():
