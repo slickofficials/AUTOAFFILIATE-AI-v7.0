2bfa6eb1748b5f5e6654fe29694a2aabcb56b675
slickofficials <158083468+slickofficials@users.noreply.github.com>
2025-10-30 13:22:47 +0100

    Update main.py

--- saas/main.py
+++ saas/main.py
@@ -1,10 +1,11 @@
+# saas/main.py
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
-main_bp = Blueprint('main', __name__)
+main_bp = Blueprint('saas_main', __name__)
 
-@main_bp.route('/')
+@main_bp.route('/dashboard')
 @login_required
 def dashboard():
     company = current_user.subdomain.upper() + " HQ"
-    return render_template('saas/dashboard.html', company=company, user=current_user) 
+    return render_template('saas/dashboard.html', company=company, user=current_user)
